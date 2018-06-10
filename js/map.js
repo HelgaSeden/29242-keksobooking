de1d777e0c74940a736eabb3c2cd767c5f33d4f9
Olga <helga.seden@gmail.com>
2018-06-10 22:40:46 +0700

    Задание по личному проекту

--- js/map.js
+++ js/map.js
@@ -59,14 +59,16 @@ var PHOTOS = [
 ];
 
 function shuffle(a) {
-  var j, x, i;
+  var j;
+  var x;
+  var i;
   for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
   }
-  return a;
+  return a
 };
 
 var getAvatar = function (n) {
